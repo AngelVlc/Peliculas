5d288470fd49780cba0e55450694c13fe68952e5
Angel Ballesteros <angelotebh@hotmail.com>
2017-07-01 12:12:03 +0200

    Habilito CORS.

--- main.js
+++ main.js
@@ -23,6 +23,12 @@ app.set('etag', false);
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // use morgan to log requests to the console
 app.use(morgan('dev'))
 
